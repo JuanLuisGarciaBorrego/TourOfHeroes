01071616417140fe88b7d66e79593c154d199bc9
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-10-23 12:31:02 +0200

    ngModel template app.component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 //Import my components
 import { AppComponent } from './component/app.component';
