17f36f5af4c62ed4ae25e603b1bb9c8801d835e2
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-10-23 17:57:16 +0200

    new component, and class model hero+

--- app/component/app.component.ts
+++ app/component/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from '../model/hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -21,14 +18,6 @@ const HEROES: Hero[] = [
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
-            <div *ngIf="selectedHero">
-              <h2>{{selectedHero.name}} details!</h2>
-              <div><label>id: </label>{{selectedHero.id}}</div>
-              <div>
-                <label>name: </label>
-                <input [(ngModel)]="selectedHero.name" placeholder="name">
-              </div>
-            </div>
             <h2>My Heroes</h2>
             <ul class="heroes">
               <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
