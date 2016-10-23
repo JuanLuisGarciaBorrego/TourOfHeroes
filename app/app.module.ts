41b887b97f1e965c4ad2ceacf0072392631a0091
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-10-23 12:27:42 +0200

    FormsModule added to app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 //Import my components
 import { AppComponent } from './component/app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
