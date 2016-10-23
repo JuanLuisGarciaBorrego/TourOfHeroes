a953e0a8cf19a2e232d1343f50c7a2c8e4bb81dc
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-10-23 17:47:54 +0200

    event click, style

--- app/component/app.component.ts
+++ app/component/app.component.ts
@@ -21,16 +21,17 @@ const HEROES: Hero[] = [
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
-            <h2>{{hero.name}} details!</h2>
-            <div><label>id: </label>{{hero.id}}</div>
-            <div>
-              <label>name: </label>
-              <input [(ngModel)]="hero.name" placeholder="name">
+            <div *ngIf="selectedHero">
+              <h2>{{selectedHero.name}} details!</h2>
+              <div><label>id: </label>{{selectedHero.id}}</div>
+              <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name">
+              </div>
             </div>
-
             <h2>My Heroes</h2>
             <ul class="heroes">
-              <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+              <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
                 <span class="badge">{{ hero.id }}</span> {{ hero.name }}
               </li>
             </ul>
@@ -86,14 +87,11 @@ const HEROES: Hero[] = [
       `]
 })
 export class AppComponent { 
-  title = 'Tour of Herores';
+  title:string = 'Tour of Herores';
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero :Hero;
 
-  onSelect(hero) {
-    
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 }
